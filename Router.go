388618a3dc2dad916c5bff803261ca45e9a7f891
Robert <rhinh25@gmail.com>
2018-11-12 23:06:06 -0500

    Added MUX library to route requests
    Playing around with this library

--- Router.go
+++ Router.go
@@ -1,4 +1,4 @@
-package router
+package main
 
 import (
 	"fmt"
@@ -7,22 +7,24 @@ import (
 )
 
 func index_handler(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(http.StatusOK)
 	fmt.Fprintf(writer, "Welcome home!")
 }
 
-func about_handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "About us!")
-}
 
-func money_handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "MOONEY")
+func user_handler(writer http.ResponseWriter, request *http.Request) {
+	param_vars := mux.Vars(request)
+	writer.WriteHeader(http.StatusOK) //HTTP status
+
+	fmt.Fprintf(writer, "Hello user number %v", param_vars["id"])
 }
 
 func main() {
-	//initiate the variable
+	//Initiate the variable
 	route := mux.NewRouter()
-	route.HandleFunc("/", index_handler)
-	route.HandleFunc("/about", about_handler)
-	route.HandleFunc("/money", money_handler)
 
+	route.HandleFunc("/", index_handler)
+	route.HandleFunc("/user/{id}", user_handler)
+	http.Handle("/", route)
+	http.ListenAndServe(":8000", nil)
 }
